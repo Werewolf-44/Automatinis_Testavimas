8a963b6809243358e4ad82a3b1e5cf2f7e480732
Ernestas Švedas <ernestas.svedas@gmail.com>
2023-11-05 12:51:32 +0200

    Add Calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Calculator
+    {
+        static internal int Sum(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        static internal int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        static internal int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        static internal double Divide(int number1, int number2)
+        {
+            return (double)number1 / number2;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 5;
+            int number2 = 2;
+
+            if (Calculator.Multiply(number1, number2) == 10)
+            {
+                Console.WriteLine($"Multiplication of {number1} and {number2} works as expected!");
+            }
+            else
+            {
+                Console.WriteLine($"Error when multiplying {number1} and {number2}!");
+            }
+        }
+    }
+}
