cc8e9c1f67ccad8fbc8b53cb507c51c2f366fb8f
Ernestas Švedas <ernestas.svedas@gmail.com>
2023-11-05 17:09:43 +0200

    Complete task 18

---
+++ SeleniumTestsWithoutPOM/GoogleTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestsWithoutPOM
+{
+    public class GoogleTests
+    {
+        [Test]
+        public void SearchGoogle()
+        {
+            //Arrange
+            string expectedResults = "Hello World! - „Google“ paieška";
+
+            //Acts
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://www.google.com";
+
+            IWebElement buttonDeclineCookies = driver.FindElement(By.XPath("//*[@id=\"W0wltc\"]"));
+            buttonDeclineCookies.Click();
+
+            IWebElement inputSearch = driver.FindElement(By.XPath("//*[@id=\"APjFqb\"]"));
+            inputSearch.SendKeys("Hello World!");
+
+            IWebElement buttonSearch = driver.FindElement(By.XPath("(//*[@name='btnK'])[2]"));
+            buttonSearch.Click();
+
+            string actualResult = driver.Title;
+
+            driver.Quit();
+
+            //Assert
+            Assert.AreEqual(expectedResults, actualResult);
+        }
+
+    }
+}
---
+++ SeleniumTestsWithoutPOM/SeleniumEasyTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestsWithoutPOM
+{
+    internal class SeleniumEasyTests
+    {
+        [Test]
+        public void SingleInputFieldTest()
+        {
+            string expectedMessage = "hello";
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
+
+            var inputEnterMessage = driver.FindElement(By.XPath("//*[@id='get-input']//input[@id='user-message']"));
+            inputEnterMessage.SendKeys(expectedMessage);
+
+            var buttonShowMessage = driver.FindElement(By.XPath("//*[@id=\"get-input\"]//button"));
+            buttonShowMessage.Click();
+
+            var spanMesssage = driver.FindElement(By.XPath("//*[@id=\"display\"]"));
+            string actualResult = spanMesssage.Text;
+
+            driver.Quit();
+
+            Assert.AreEqual(expectedMessage, actualResult);
+        }
+    }
+}
